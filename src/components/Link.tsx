5d57a2e4e2347d2084890b0cc7f645407f0c4120
Takafumi Narise <tn@Takafumis-MacBook-Air.local>
2016-04-17 13:59:15 +0900

    first

---
+++ src/components/Link.tsx
@@ -0,0 +1,26 @@
+import React, { PropTypes } from 'react'
+
+export interface LinkProps extends React.Props<any> {
+  active: boolean,
+  children: any,
+  onClick: any
+}
+
+export default class Link extends React.Component<LinkProps, any> {
+  if (active) {
+    return <span>{this.props.children}</span>
+  }
+  
+  render() {
+    return (
+      <a href="#"
+         onClick={e => {
+           e.preventDefault()
+           this.props.onClick()
+         }}
+      >
+        {this.props.children}
+      </a>
+    )
+  }
+}
---
+++ src/components/Todo.tsx
@@ -0,0 +1,22 @@
+import React, { PropTypes } from 'react'
+
+export interface TodoProps extends React.Props<any> {
+  onClick: any;
+  completed: boolean;
+  text: string;
+}
+
+export default class Todo extends React.Component<TodoProps, any> {
+  render() {
+    return (
+      <li
+        onClick={this.props.onClick}
+        style={ {
+            textDecoration: this.props.completed ? 'line-through' : 'none'
+        } }
+      >
+        {this.props.text}
+      </li>
+    );
+  }
+}
